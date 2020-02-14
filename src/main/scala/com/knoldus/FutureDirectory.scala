7defef96cf9f607ffe209acee9c6ae2f7bffcc23
ShivaniMehrotra4 <shivanimehrotra.sms@gmail.com>
2020-02-14 09:59:16 +0530

    updated source code

--- src/main/scala/com/knoldus/FutureDirectory.scala
+++ src/main/scala/com/knoldus/FutureDirectory.scala
@@ -4,29 +4,34 @@ import java.io.File
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-//import scala.util.{Failure, Success}
+import scala.util.{Failure, Success}
 
 
 object FutureDirectory extends App {
 
-  def listDirectories(pathDirectory: String): List[String] = {
+  /**
+   * Create a application which takes folder path as input and print all the names of files in a directory using Scala futures.
+   * @param pathDirectory-A string path of a file/directory
+   */
+  def listDirectories(pathDirectory: String): Unit = {
     val path: File = new File(pathDirectory)
     val listOfFiles = path.listFiles
-    val l1 = List.empty[String]
+    //val l1 = List.empty[String]
     for (temporaryList <- listOfFiles)
-      if (temporaryList.isFile)
-      {
-        l1 +: temporaryList.getPath
+      if (temporaryList.isFile) {
+        println(temporaryList.getPath)
+        //        l1 +: temporaryList.getPath
+
       }
       else
         Future {
           listDirectories(temporaryList.getPath)
         }
-    l1
+
   }
 
   val path = "/home/knoldus/Music"
-  val l2: List[String] = listDirectories(path)
+  listDirectories(path)
 
   //  l2 onComplete {
   //    case Success(res) => res
